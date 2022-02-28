11f9af37868e915138061de8ef67fc302f2b30d8
Caleb Elson <elsonc@oregonstate.edu>
2022-02-28 15:03:42 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,157 @@
+# Usage: Write text to file "io.txt" in the folder where this file is running
+# Start message with language code from the dictionary at the bottom of the file
+# If main() finds a matching code, it translates the message and writes to file
+# Sleeps for 5 seconds and checks again
+
+from time import sleep
+from pathlib import Path
+from translatepy.translators.google import GoogleTranslate as Translator
+io_address = "./io.txt"
+translator = Translator()
+
+
+def translate_input(input_text, language):
+    return translator.translate(input_text, language).result
+
+
+def write_translation(translated):
+    with open(io_address, 'w') as io_file:
+        io_file.write(translated)
+
+
+def get_input():
+    with open(io_address, "r") as io_file:
+        return io_file.read()
+
+
+def main():
+    Path(io_address).touch(exist_ok=True)
+
+    while True:
+        io_input = get_input()
+
+        try:
+            io_input = str(io_input)
+            language = io_input.split()[0]
+
+            # If input language found, split from input string
+            # Else raise exception
+            if language in LANGUAGES:
+                io_input = io_input.split(" ", 1)[1]
+            else:
+                raise IndexError
+
+            write_translation(translate_input(io_input, language))
+        except IndexError:
+            pass
+
+        sleep(5)
+
+
+LANGUAGES = {
+    'af': 'afrikaans',
+    'sq': 'albanian',
+    'am': 'amharic',
+    'ar': 'arabic',
+    'hy': 'armenian',
+    'az': 'azerbaijani',
+    'eu': 'basque',
+    'be': 'belarusian',
+    'bn': 'bengali',
+    'bs': 'bosnian',
+    'bg': 'bulgarian',
+    'ca': 'catalan',
+    'ny': 'chichewa',
+    'co': 'corsican',
+    'hr': 'croatian',
+    'cs': 'czech',
+    'da': 'danish',
+    'nl': 'dutch',
+    'en': 'english',
+    'eo': 'esperanto',
+    'et': 'estonian',
+    'tl': 'filipino',
+    'fi': 'finnish',
+    'fr': 'french',
+    'fy': 'frisian',
+    'gl': 'galician',
+    'ka': 'georgian',
+    'de': 'german',
+    'el': 'greek',
+    'gu': 'gujarati',
+    'ht': 'haitian creole',
+    'ha': 'hausa',
+    'iw': 'hebrew',
+    'he': 'hebrew',
+    'hi': 'hindi',
+    'hu': 'hungarian',
+    'is': 'icelandic',
+    'ig': 'igbo',
+    'id': 'indonesian',
+    'ga': 'irish',
+    'it': 'italian',
+    'ja': 'japanese',
+    'jw': 'javanese',
+    'kn': 'kannada',
+    'kk': 'kazakh',
+    'km': 'khmer',
+    'ko': 'korean',
+    'ku': 'kurdish (kurmanji)',
+    'ky': 'kyrgyz',
+    'lo': 'lao',
+    'la': 'latin',
+    'lv': 'latvian',
+    'lt': 'lithuanian',
+    'lb': 'luxembourgish',
+    'mk': 'macedonian',
+    'mg': 'malagasy',
+    'ms': 'malay',
+    'ml': 'malayalam',
+    'mt': 'maltese',
+    'mi': 'maori',
+    'mr': 'marathi',
+    'mn': 'mongolian',
+    'my': 'myanmar (burmese)',
+    'ne': 'nepali',
+    'no': 'norwegian',
+    'or': 'odia',
+    'ps': 'pashto',
+    'fa': 'persian',
+    'pl': 'polish',
+    'pt': 'portuguese',
+    'pa': 'punjabi',
+    'ro': 'romanian',
+    'ru': 'russian',
+    'sm': 'samoan',
+    'gd': 'scots gaelic',
+    'sr': 'serbian',
+    'st': 'sesotho',
+    'sn': 'shona',
+    'sd': 'sindhi',
+    'si': 'sinhala',
+    'sk': 'slovak',
+    'sl': 'slovenian',
+    'so': 'somali',
+    'es': 'spanish',
+    'su': 'sundanese',
+    'sw': 'swahili',
+    'sv': 'swedish',
+    'tg': 'tajik',
+    'ta': 'tamil',
+    'te': 'telugu',
+    'th': 'thai',
+    'tr': 'turkish',
+    'uk': 'ukrainian',
+    'ur': 'urdu',
+    'ug': 'uyghur',
+    'uz': 'uzbek',
+    'vi': 'vietnamese',
+    'cy': 'welsh',
+    'xh': 'xhosa',
+    'yi': 'yiddish',
+    'yo': 'yoruba',
+    'zu': 'zulu'
+}
+
+if __name__ == '__main__':
+    main()
